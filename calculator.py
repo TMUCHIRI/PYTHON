c3fedf61440d6be2c8908d3961a245629625823c
DEV TIM <developer@dreamonwheels.com>
2025-08-21 15:12:13 +0300

    file name changed to calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# CALCULATOR APP
+
+operator = input("Enter the operator (+, -, *, /): ")
+if operator not in ('+', '-', '*', '/'):
+    print("Invalid operator")
+else:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if operator == '+':
+        def add_numbers(num1, num2):
+            return num1 + num2
+        print("The sum is:", add_numbers(num1, num2))
+    elif operator == '-':
+        def subtract_numbers(num1, num2):
+            return num1 - num2
+        print("The difference is:", subtract_numbers(num1, num2))
+    elif operator == '*':
+        def multiply_numbers(num1, num2):
+            return num1 * num2
+        print("The product is:", multiply_numbers(num1, num2))
+    elif operator == '/':
+        def divide_number(num1, num2):
+            return num1 / num2
+        print("The quotient is:", divide_number(num1, num2))
