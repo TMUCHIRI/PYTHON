b28c1e159486c35b4a233879ec3933960aafb1da
DEV TIM <developer@dreamonwheels.com>
2025-08-25 08:54:31 +0300

    classes and inheritance

---
+++ classes.py
@@ -0,0 +1,11 @@
+#class Myclass():
+#    name = input("Enter your name: ")
+#    age = int(input("Enter your age: "))
+#    gender = input("Enter your gender: ")
+
+#using init function
+class Myclass():
+    def __init__(self):
+        self.name = input("Enter your name: ")
+        self.age = int(input("Enter your age: "))
+        self.gender = input("Enter your gender: ")
---
+++ inheritance.py
@@ -0,0 +1,9 @@
+from classes import Myclass
+
+class Person(Myclass):
+    pass
+
+obj = Person()
+print('Hi ',obj.name)
+print('You are ',obj.age,' years old')
+print('You are a ',obj.gender)
