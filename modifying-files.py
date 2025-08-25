13ec6d3b015bb11066629f5c8761c68f559ea91c
DEV TIM <developer@dreamonwheels.com>
2025-08-25 08:46:01 +0300

    writing files in python

--- modifying-files.py
+++ modifying-files.py
@@ -14,6 +14,19 @@ country_file = open('countries.txt', 'r')
 #print(country_file.readlines())
 
 #Using a loop to read contents of a file
-for content in country_file.readlines():
-    print(content)
-country_file.close()
+#for content in country_file.readlines():
+#    print(content)
+#country_file.close()
+
+#Writing and appending files
+#new_file = open('new_file.txt', 'w')
+#new_file.write("This is a new line added to the file.\n")
+
+#new_file = open('new_file.txt', 'a')
+#new_file.write("This line is appended to the file.\n")
+
+#creating a python file 
+#new_file = open('new_file.py', 'w')
+#new_file.write('print("This is a python file created by another python file")')
+
+#new_file.close()
